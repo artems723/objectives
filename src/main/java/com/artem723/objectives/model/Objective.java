709115fd30a806bd242212a197a562d77e500008
artem723 <email@email.com>
2016-10-03 15:15:21 +0300

    first commit

---
+++ src/main/java/com/artem723/objectives/model/Objective.java
@@ -0,0 +1,9 @@
+package com.artem723.objectives.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Objective extends NamedEntity {
+
+
+}
---
+++ src/main/java/com/artem723/objectives/model/Role.java
@@ -0,0 +1,6 @@
+package com.artem723.objectives.model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
