c6c5f0a2bddd23f6b2f45b4b9df37e491b14c65a
artem723 <artem.staver@gmail.com>
2016-10-04 17:28:49 +0300

    added Repository interfaces

---
+++ src/main/java/com/artem723/objectives/repository/ObjectiveRepository.java
@@ -0,0 +1,23 @@
+package com.artem723.objectives.repository;
+
+import com.artem723.objectives.model.Objective;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+public interface ObjectiveRepository {
+    // null if updated objective do not belong to userId
+    Objective save(Objective objective, int userId);
+
+    // false if objective do not belong to userId
+    boolean delete(int id, int userId);
+
+    // null if objective do not belong to userId
+    Objective get(int id, int userId);
+
+    // ORDERED name
+    Collection<Objective> getAll(int userId);
+
+//    // ORDERED dateTime
+//    Collection<Objective> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId);
+}
---
+++ src/main/java/com/artem723/objectives/repository/RecordRepository.java
@@ -0,0 +1,19 @@
+package com.artem723.objectives.repository;
+
+import com.artem723.objectives.model.Record;
+
+import java.util.Collection;
+
+public interface RecordRepository {
+    // null if updated record do not belong to objectiveId and userId
+    Record save(Record record, int objectiveId, int userId);
+
+    // false if record do not belong to objectiveId and userId
+    boolean delete(int id, int objectiveId, int userId);
+
+    // null if record do not belong to objectiveId and userId
+    Record get(int id, int objectiveId, int userId);
+
+    // ORDERED date
+    Collection<Record> getAll(int objectiveId, int userId);
+}
